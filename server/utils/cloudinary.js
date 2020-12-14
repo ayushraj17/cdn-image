23c6f4419b5f0ab458d03630e8c2333b41e57153
ayushraj17 <ayushraj@classcast.in>
2020-12-14 22:45:20 +0530

    sd

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+	api_key: "669411354282412",
+	api_secret: "P7QUr1u49MtJKOUTY_t-rRKbhZo",
+	cloud_name: "ayushraj",
+	// cloud_name: process.env.CLOUDINARY_NAME,
+	// api_key: process.env.CLOUDINARY_API_KEY,
+	// api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = { cloudinary };
